1fa04f02f630cb3d29fb64bae5a79c13705fb079
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-27 22:39:09 +0530

    server running

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import { Express } from "express";
+import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -7,4 +7,16 @@ import cors from "cors";
 dotenv.config()
 
 //initialize application
-const app = Express();
+const app = express();
+
+//import port no. in the env file
+const PORT = process.env.PORT || 3000;
+//fallback - 3000 if 8000 port isn't working
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on ${PORT}`);
+})
