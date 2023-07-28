b2d7d76292259de557155214b4f0231d0aae7b77
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-28 20:55:28 +0530

    check if the user exists

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,16 @@ import { prisma } from "../config/PrismaConfig.js"
 export const createUser = asyncHandler(async(req,res) => {
     console.log("creating a user");
 
-    let {email} = req.body;
+    let {email} = req.body; 
 
-    console.log(email);
+    //checking user is exisiting, if not create an account
+    const userExists = await prisma.user.findUnique({where: {email: email}})
+    if(!userExists) {
+        const user = await prisma.user.create({data: req.body});
+        res.send ({
+            message: "User registered successfully",
+            user: user,
+        });
+    }else res.send(201).json({message: "User already exist"});
+
 });
