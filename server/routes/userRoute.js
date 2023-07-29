5c96de4750fb6470a987f49b53c7ad90dad3b517
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-29 13:48:45 +0530

    create endpoints of bookVisit

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import { bookVisit, cancelBooking, createUser, getAllBookings } from "../controllers/userController.js";
 
 //making an express object
 const router = express.Router()
 router.post ("/register", createUser); //create the user inside the usercontroller.js
+router.post("/bookVisit/:id", bookVisit); //allocate booking based on residence id
+router.get("//allBookings", getAllBookings);
+router.post("/removeBooking/:id", cancelBooking);
 
 export { router as userRoute}
