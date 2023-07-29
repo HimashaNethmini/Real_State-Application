bc3d32909d15722410bfa33367d4f6b73958af93
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-29 10:36:33 +0530

    endpoint to filter by id

--- server/routes/ResidenceRoute.js
+++ server/routes/ResidenceRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createResidence, getAllResidency } from "../controllers/residenceController.js";
+import { createResidence, getAllResidencies, getAllResidency } from "../controllers/residenceController.js";
 const router = express.Router();
 
 //endpoint
 router.post("/create", createResidence)
-router.get("/allread", getAllResidency)
+router.get("/allread", getAllResidency) //all
+router.get("/:id", getAllResidencies) //provide details filter by id
 
 export { router as residenceRoute}
