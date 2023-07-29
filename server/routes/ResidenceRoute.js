e85f96046d86493c9f257274ced7d32a4413c9f8
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-29 10:18:27 +0530

    create the endpoints to get all

--- server/routes/ResidenceRoute.js
+++ server/routes/ResidenceRoute.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { createResidence } from "../controllers/residenceController.js";
+import { createResidence, getAllResidency } from "../controllers/residenceController.js";
 const router = express.Router();
 
 //endpoint
 router.post("/create", createResidence)
+router.get("/allread", getAllResidency)
+
 export { router as residenceRoute}
