02cdc48753374f88d68e28495e14088fd99a2055
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-15 22:22:20 +0530

    change the header colour

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <>
-      <div>
+      <div style={{background: "var(--black)", overflow: "hidden"}}>
         <Header />
+        <Outlet/>
       </div>
       <Footer />
     </>
