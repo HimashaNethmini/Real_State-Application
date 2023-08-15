2dbf96088da6f8bd1c15904cb0ac2219310065ec
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-15 22:41:39 +0530

    Add Properties to app.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import React, { Suspense } from "react";
 import "./App.css";
 import Website from "./pages/Website";
-import Layout from "./components/Layout/Layout";
+import Layout from "./components/Layout/Layout"; 
+import properties from "./pages/Properties/Properties";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Properties from "./pages/Properties/Properties";
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Routes>
           <Route element = {<Layout />}>
             <Route path="/" element={<Website />} />
+            <Route path = "/properties" element={<Properties />}/>
           </Route>
         </Routes>
       </Suspense>
