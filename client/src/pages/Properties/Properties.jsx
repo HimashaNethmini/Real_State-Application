4e4709a807251019224dceca03696a5c10d549a4
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-17 20:14:09 +0530

    Add search bar and design it

--- client/src/pages/Properties/Properties.jsx
+++ client/src/pages/Properties/Properties.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Properties.css';
+import SearchBar from "../../components/SearchBar/SearchBar"
 
 const Properties = () => {
   return (
     <div className='wrapper'>
+      <div className="flexCenter paddings innerWidth properties-container">
+        <SearchBar/>
+      </div>
       
     </div>
   )
