ce3a2ff6bb2d529a472b549de277385ebd054812
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-17 21:04:26 +0530

    Make useProperties hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,27 @@
 import React, { Suspense } from "react";
 import "./App.css";
 import Website from "./pages/Website";
-import Layout from "./components/Layout/Layout"; 
-import properties from "./pages/Properties/Properties";
+import Layout from "./components/Layout/Layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Properties from "./pages/Properties/Properties";
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <Suspense fallback={<div>Loading.....</div>}>
-        <Routes>
-          <Route element = {<Layout />}>
-            <Route path="/" element={<Website />} />
-            <Route path = "/properties" element={<Properties />}/>
-          </Route>
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading.....</div>}>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Website />} />
+              <Route path="/properties" element={<Properties />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- client/src/pages/Properties/Properties.jsx
+++ client/src/pages/Properties/Properties.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Properties.css';
 import SearchBar from "../../components/SearchBar/SearchBar"
+import useProperties from '../../hooks/useProperties';
 
 const Properties = () => {
+
+  const { data, isError, isLoading } = useProperties()
   return (
     <div className='wrapper'>
       <div className="flexCenter paddings innerWidth properties-container">
