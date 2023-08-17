abc911a18dfb788c461e112d8497dd6d10c25214
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-17 21:07:43 +0530

    Add axios, dayjs and react toast

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Layout from "./components/Layout/Layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Properties from "./pages/Properties/Properties";
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const queryClient = new QueryClient();
@@ -21,6 +22,7 @@ function App() {
           </Routes>
         </Suspense>
       </BrowserRouter>
+      <ToastContainer/>
     </QueryClientProvider>
   );
 }
---
+++ client/src/utils/api.js
@@ -0,0 +1,3 @@
+import axios from 'axios';
+import dayjs from 'dayjs';
+import { toast } from 'react-toastify';
