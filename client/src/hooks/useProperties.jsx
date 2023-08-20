9b8c7c64faebcb021241154c7944b213c7948940
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-20 11:14:25 +0530

    import get all properties

--- client/src/hooks/useProperties.jsx
+++ client/src/hooks/useProperties.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useQuery } from "react-query";
-//import { getAllProperties } from "../utils/api";
+import { getAllProperties } from "../utils/api";
 
 const useProperties = () => {
   const { data, isLoading, isError, refetch } = useQuery(
