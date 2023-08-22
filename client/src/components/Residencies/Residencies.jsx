e75a744f69a9dd4e4dfe42705a49eb6e84d766eb
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-22 15:58:34 +0530

    Fetch data from db

--- client/src/components/Residencies/Residencies.jsx
+++ client/src/components/Residencies/Residencies.jsx
@@ -1,13 +1,38 @@
 import React from "react";
-import data from "../../utils/slider.json";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "./Residencies.css";
 import { sliderSettings } from "../../utils/common";
 import PropertyCard from "../PropertyCard/PropertyCard";
+import useProperties from "../../hooks/useProperties";
+import { PuffLoader } from "react-spinners";
 
 const Residencies = () => {
+  const { data, isError, isLoading } = useProperties();
+
+  if (isError) {
+    return(
+      <div className="wrapper">
+        <span>Error while fetching data</span>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="wrapper flexCenter" style={{height: "60vh"}}>
+        <PuffLoader
+        height = "80"
+        width = "80"
+        radius={1}
+        color='#4066ff'
+        aria-label='puff-loading' />
+
+      </div>
+    );
+  }
+
   return (
     <div id="residencies" className="r-wrapper">
       <div className="paddings innerWidth r-container">
@@ -19,7 +44,7 @@ const Residencies = () => {
           <SlideNextButton />   {/*add slider buttons*/}
           
           {/* make the slider */}
-          {data.map((card, i) => (
+          {data.slice(0, 8).map((card, i) => (
             <SwiperSlide key={i}>
               <PropertyCard card={card}/>
             </SwiperSlide>
