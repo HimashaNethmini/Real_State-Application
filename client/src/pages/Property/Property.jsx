8bb3ba2d243734cb25ff22e613608581d46a5c56
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-23 11:24:23 +0530

    imported puff loader

--- client/src/pages/Property/Property.jsx
+++ client/src/pages/Property/Property.jsx
@@ -3,7 +3,7 @@ import { useMutation, useQuery } from "react-query";
 import { useLocation } from "react-router-dom";
 import { getProperty, removeBooking } from "../../utils/api";
 import { PuffLoader } from "react-spinners";
-import { AiFillHeart } from "react-icons/ai";
+//import { AiFillHeart } from "react-icons/ai";
 import "./Property.css";
 
 import { FaShower } from "react-icons/fa";
