0c3a9af18af5c21994a75cee5a7920390ab35c98
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-25 20:09:05 +0530

    adding props

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 
 
-const Map = () => {
+const Map = ({address, city,country}) => {
   return (
-    <MapContainer>
+    <MapContainer
+    center ={[53.35, 18.8]}
+    zoom={1}
+    scrollWheelZoom={false}
+    style={{
+        height: "40vh",
+        width: "100%",
+        marginTop: "20px",
+        zIndex: 0,
+       
+    }}>
+
         <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'/>
         <GeoCoderMarker address={`${address} ${city} ${country}`} />
     </MapContainer>
