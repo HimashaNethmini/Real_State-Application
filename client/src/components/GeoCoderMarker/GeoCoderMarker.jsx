11b5c6cd6c1de61cb751708f5c01d7e5e6879728
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-25 20:25:35 +0530

    marker position according to address

--- client/src/components/GeoCoderMarker/GeoCoderMarker.jsx
+++ client/src/components/GeoCoderMarker/GeoCoderMarker.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Marker, Popup, useMap } from 'react-leaflet';
+import L from 'leaflet';
+import "leaflet/dist/leaflet.css"
+import icon from "leaflet/dist/images/marker-shadow.png"
+import * as ELG from 'esri-leaflet-geocoder'
 
-let DefaultIcon = L.icon ({
+let DefaulIcon = L.icon ({
     iconUrl: icon,
     shadowUrl: iconShadow
 })
-L.Marker.prototype.options.icon = DefaultIcon
+L.Marker.prototype.options.icon = DefaulIcon
+
+
 const GeoCoderMarker = ({address}) => {
 
     // adding the location for the marker in the map
     const map = useMap()
     const [ position, setposition ] = useState([60, 19]) 
+
+    useEffect(()=> {
+        ELG.geocode().text(address).run((err, results, response)=> {
+            if(results?.results?.length > 0){
+                const {lat, lng} = results?.results[0].latlng
+                setPosition([lat, lng])
+                map.flyTo([lat, lng], 6)
+            }
+        })
+    }, [address])
+
   return (
-    <Marker position={position} icon={DefaultIcon} >
-        <Popup />
+    <Marker position={position} icon={DefaulIcon}>
+        <Popup/>
     </Marker>
   )
 }
