8776d56e83af3939cd1f664f65148a8f11a72dec
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-25 13:00:01 +0530

    adding map location to single property page

--- client/src/pages/Property/Property.jsx
+++ client/src/pages/Property/Property.jsx
@@ -9,7 +9,7 @@ import "./Property.css";
 import { FaShower } from "react-icons/fa";
 import { AiTwotoneCar } from "react-icons/ai";
 import { MdLocationPin, MdMeetingRoom } from "react-icons/md";
-//import Map from "../../components/Map/Map";
+import Map from "../../components/Map/Map";
 //import useAuthCheck from "../../hooks/useAuthCheck";
 //import { useAuth0 } from "@auth0/auth0-react";
 //import BookingModal from "../../components/BookingModal/BookingModal";
@@ -71,7 +71,7 @@ const Property = () => {
       <div className="flexColStart paddings innerWidth property-container">
 
         {/* like button */}
-        
+
         <div className="like">
           <Heart id={id}/>
         </div>
