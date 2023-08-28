781585016cee9eeaf09b0c3931312414e4579dde
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-28 21:23:29 +0530

    users details stored in db

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import { Outlet } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import UserDetailContext from "../../context/UserDetailContext";
+import { useMutation } from "react-query";
+import { createUser } from "../../utils/api";
+
 
 const Layout = () => {
+
+  const {isAuthenticated, user} = useAuth0()
+  const { setUserDetails} = useContext(UserDetailContext)
+
+  const {mutate} = useMutation({
+    mutationKey: [user?.email],
+    mutationFn: () => createUser(user?.email)
+  })
+
+  useEffect(()=> {
+    isAuthenticated && mutate()
+  }, [isAuthenticated])
+
   return (
     <>
       <div style={{background: "var(--black)", overflow: "hidden"}}>
