29bd3e3383b7a8a8c32b8d29db29a3a39260427f
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-28 20:15:21 +0530

    updated user profile

--- client/src/components/ProfileMenu/ProfileMenu.jsx
+++ client/src/components/ProfileMenu/ProfileMenu.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import { Avatar, Menu } from '@mantine/core'
+import { useNavigate } from 'react-router-dom';
 
-const ProfileMenu = ( {user, logout}) => {
-  return (
-    <Menu>
+const ProfileMenu = ({user, logout}) => {
+  const navigate = useNavigate()
+return (
+  <Menu>
       <Menu.Target>
-        <Avatar src= {user?.picture} alt='user image' radius={"xl"} />
+          <Avatar src={user?.picture} alt='user image' radius={"xl"}/>
       </Menu.Target>
       <Menu.Dropdown>
-        <Menu.Item>
-          Favourites
-        </Menu.Item>
+          <Menu.Item onClick={()=> navigate("./favourites", {replace: true})}>
+              Favourites
+          </Menu.Item>
+
+          <Menu.Item onClick={()=> navigate("./bookings", {replace: true})}>
+              Bookings
+          </Menu.Item>
+
+          <Menu.Item onClick={()=>{
+              localStorage.clear();
+              logout()
+          }}>
+              Logout
+          </Menu.Item>
       </Menu.Dropdown>
-    </Menu>
-  )
+  </Menu>
+)
 }
+
 
 export default ProfileMenu
