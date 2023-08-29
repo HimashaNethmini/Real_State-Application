894f9c9462d7d6a5087e5c5e082eef5c39c7d884
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-29 12:19:05 +0530

    email is send with the token to access

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -10,16 +10,30 @@ import { createUser } from "../../utils/api";
 
 const Layout = () => {
 
-  const {isAuthenticated, user} = useAuth0()
+  const {isAuthenticated, user, getAccessTokenWithPopup} = useAuth0()
   const { setUserDetails} = useContext(UserDetailContext)
 
   const {mutate} = useMutation({
     mutationKey: [user?.email],
-    mutationFn: () => createUser(user?.email)
+    mutationFn: () => createUser(user?.email, token)
   })
 
   useEffect(()=> {
-    isAuthenticated && mutate()
+
+    const getTokenAndRegister = async ()=> {
+
+      const res = await getAccessTokenWithPopup({
+        authorizationParams: {
+          audience: "http://localhost:8000",
+          scope: "openid profile email"
+        }
+      });
+      localStorage.setItem("access_token", res);
+      setUserDetails((prev)=> ({...prev, token: res }));
+      mutate(res)
+    }
+
+    isAuthenticated && getTokenAndRegister()
   }, [isAuthenticated])
 
   return (
