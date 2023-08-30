4e97a0c8aa9bd226f5e989f507b37da395f8bf93
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-30 12:01:16 +0530

    securing creating with jwt

--- server/routes/ResidenceRoute.js
+++ server/routes/ResidenceRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { createResidency, getAllResidencies, getResidency } from "../controllers/residenceController.js";
+import jwtCheck from "../config/auth0Configure.js";
+
 const router = express.Router();
 
 //endpoint
-router.post("/create", createResidency)
+router.post("/create", jwtCheck, createResidency)
 router.get("/allresd", getAllResidencies) //all
 router.get("/:id", getResidency) //provide details filter by id
 
