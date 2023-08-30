eedccce572026e8fc60add79d8860a20ff750413
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-30 12:00:19 +0530

    securing endpoints using jwtcheck

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,14 +1,16 @@
 import express from "express";
 import { bookVisit, cancelBooking, createUser, getAllBookings, getAllFavourites, toFav } from "../controllers/userController.js";
 import jwtCheck from "../../server/config/auth0Configure.js";
+
 //making an express object
 const router = express.Router()
+
 router.post ("/register", jwtCheck, createUser); //create the user inside the usercontroller.js
-router.post("/bookVisit/:id", bookVisit); //allocate booking based on residence id
+router.post("/bookVisit/:id", jwtCheck, bookVisit); //allocate booking based on residence id
 router.post("//allBookings", getAllBookings);
-router.post("/removeBooking/:id", cancelBooking);
+router.post("/removeBooking/:id", jwtCheck, cancelBooking);
 
 //favourite routes
-router.post("/favadd/:rid", toFav);
-router.post("/getallFav", getAllFavourites);
+router.post("/favadd/:rid",jwtCheck, toFav);
+router.post("/getallFav", jwtCheck, getAllFavourites);
 export { router as userRoute}
