36d2cd638880e52338eac10834f2a568f5420e99
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-30 22:41:28 +0530

    update favourites function

--- client/src/utils/common.js
+++ client/src/utils/common.js
@@ -24,3 +24,20 @@ export const sliderSettings = {
 
 };
 
+export const updateFavourites = (id, favourites) => {
+  if (favourites.includes(id)) {
+    return favourites.filter((resId) => resId !== id);
+  } else {
+    return [...favourites, id];
+  }
+};
+
+export const checkFavourites = (id, favourites) => {
+  return favourites?.includes(id) ? "#fa3e5f" : "white";
+};
+
+export const validateString = (value) => {
+  return value?.length < 3 || value === null
+    ? "Must have atleast 3 characters"
+    : null;
+};
