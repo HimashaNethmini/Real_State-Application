0d55b56d1dc9ac84014698a9a0b16d2a2eb1db73
HimashaNethmini <himashanethmini05@gmail.com>
2023-08-31 11:03:30 +0530

    gives a list of favourite residences

--- client/src/hooks/useFavourites.jsx
+++ client/src/hooks/useFavourites.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import UserDetailContext from '../context/UserDetailContext'
 import { useQuery } from "react-query";
 import { useAuth0 } from "@auth0/auth0-react";
